f2bd21e8457f4a22fac102c358fe28273843c002
rajatabha-cs <riku.das2003@gmail.com>
2023-04-06 13:18:56 +0530

    profile commit

--- app/src/main/java/com/example/petzzu/Dashboard.kt
+++ app/src/main/java/com/example/petzzu/Dashboard.kt
@@ -1,11 +1,21 @@
 package com.example.petzzu
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.google.android.material.button.MaterialButton
 
 class Dashboard : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        val secondActbutton=findViewById<ImageView>(R.id.petfolio)
+        secondActbutton.setOnClickListener {
+            val Intent = Intent(this,Petfolio::class.java)
+            startActivity(Intent)
+        }
     }
 }
