feb22d860ad48f4a55fc48becde3276bae66b3de
rajatabha-cs <riku.das2003@gmail.com>
2023-04-06 10:54:32 +0530

    dashboard commit

--- app/src/main/java/com/example/petzzu/Signuppage.kt
+++ app/src/main/java/com/example/petzzu/Signuppage.kt
@@ -1,11 +1,18 @@
 package com.example.petzzu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 
 class Signuppage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signuppage)
+        val secondActbutton=findViewById<MaterialButton>(R.id.signbtn)
+        secondActbutton.setOnClickListener {
+            val Intent = Intent(this,Dashboard::class.java)
+            startActivity(Intent)
+        }
     }
 }
