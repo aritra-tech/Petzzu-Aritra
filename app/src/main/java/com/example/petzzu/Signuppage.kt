5fb4a4f83d4af22217f104ad5c7d9a20ebbda6e9
rajatabha-cs <riku.das2003@gmail.com>
2023-04-24 15:43:10 +0530

    firebasecommit

--- app/src/main/java/com/example/petzzu/Signuppage.kt
+++ app/src/main/java/com/example/petzzu/Signuppage.kt
@@ -3,16 +3,61 @@ package com.example.petzzu
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Signuppage : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signuppage)
+
+        auth= Firebase.auth
         val secondActbutton=findViewById<MaterialButton>(R.id.signbtn)
         secondActbutton.setOnClickListener {
-            val Intent = Intent(this,Dashboard::class.java)
-            startActivity(Intent)
+            performSignUp()
         }
+    }
+    private fun performSignUp() {
+        val username=findViewById<EditText>(R.id.user)
+        val email=findViewById<EditText>(R.id.email)
+        val password=findViewById<EditText>(R.id.password)
+        val confirmPass=findViewById<EditText>(R.id.password1)
+
+        if(email.text.isEmpty() || password.text.isEmpty() || username.text.isEmpty() ||confirmPass.text.isEmpty()){
+            Toast.makeText(this,"Please fill all fields",Toast.LENGTH_SHORT).show()
+            return
+        }
+        val inputemail=email.text.toString()
+        val inputpassword=password.text.toString()
+
+        auth.createUserWithEmailAndPassword(inputemail,inputpassword)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    val Intent = Intent(this,Dashboard::class.java)
+                    startActivity(Intent)
+
+                    Toast.makeText(this, "Authentication Successfull.",
+                        Toast.LENGTH_SHORT).show()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+
+                    Toast.makeText(this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Error Occurred ${it.localizedMessage}",
+                    Toast.LENGTH_SHORT).show()
+            }
     }
 }
