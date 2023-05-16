5f61ae16c0bd55c90875c315cd800aa267156d4b
rajatabha-cs <riku.das2003@gmail.com>
2023-05-16 13:08:01 +0530

    Post View

--- app/src/main/java/com/example/petzzu/PetCommunity.java
+++ app/src/main/java/com/example/petzzu/PetCommunity.java
@@ -1,6 +1,7 @@
 package com.example.petzzu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,15 +12,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
+import com.example.petzzu.Adapter.PostAdapter;
+import com.example.petzzu.Model.Post;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PetCommunity extends AppCompatActivity {
 
@@ -28,6 +41,10 @@ public class PetCommunity extends AppCompatActivity {
     private FirebaseFirestore firestore;
     private RecyclerView mRecyclerView;
     private FloatingActionButton fab;
+    private PostAdapter adapter;
+    private List<Post> list;
+    private Query query;
+    private ListenerRegistration listenerRegistration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +56,9 @@ public class PetCommunity extends AppCompatActivity {
         mRecyclerView=findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(PetCommunity.this));
+        list=new ArrayList<>();
+        adapter=new PostAdapter(PetCommunity.this,list);
+        mRecyclerView.setAdapter(adapter);
         fab=findViewById(R.id.floatingActionButton);
         setSupportActionBar(mainToolBar);
         getSupportActionBar().setTitle("Pet Community");
@@ -49,6 +69,35 @@ public class PetCommunity extends AppCompatActivity {
                 startActivity(new Intent(PetCommunity.this,AddPostActivity.class));
             }
         });
+
+        if (firebaseAuth.getCurrentUser()!=null){
+            mRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    Boolean isBottom=!mRecyclerView.canScrollVertically(1);
+                    if (isBottom)
+                        Toast.makeText(PetCommunity.this, "Reached the bottom", Toast.LENGTH_SHORT).show();
+                }
+            });
+            query=firestore.collection("Posts").orderBy("time",Query.Direction.DESCENDING);
+            listenerRegistration=query.addSnapshotListener(PetCommunity.this, new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    for (DocumentChange doc:value.getDocumentChanges()){
+                        if (doc.getType()==DocumentChange.Type.ADDED){
+                            Post post=doc.getDocument().toObject(Post.class);
+                            list.add(post);
+                            adapter.notifyDataSetChanged();
+                        }
+                        else {
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                    listenerRegistration.remove();
+                }
+            });
+        }
     }
 
     @Override
