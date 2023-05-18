5d170cb1319c45f387cbf362c185cfd5fd261333
rajatabha-cs <riku.das2003@gmail.com>
2023-05-18 15:25:30 +0530

    Comment View

--- app/src/main/java/com/example/petzzu/CommentsActivity.java
+++ app/src/main/java/com/example/petzzu/CommentsActivity.java
@@ -1,6 +1,7 @@
 package com.example.petzzu;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,14 +12,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.petzzu.Adapter.CommentsAdapter;
+import com.example.petzzu.Model.Comments;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CommentsActivity extends AppCompatActivity {
@@ -30,6 +39,8 @@ public class CommentsActivity extends AppCompatActivity {
     private String post_id;
     private String currentUserId;
     private FirebaseAuth auth;
+    private CommentsAdapter adapter;
+    private List<Comments> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,11 +53,31 @@ public class CommentsActivity extends AppCompatActivity {
         firestore=FirebaseFirestore.getInstance();
         auth=FirebaseAuth.getInstance();
         currentUserId=auth.getCurrentUser().getUid();
+
+        mList=new ArrayList<>();
+        adapter=new CommentsAdapter(CommentsActivity.this,mList);
+
         post_id=getIntent().getStringExtra("postid");
 
         mCommentRecyclerView.setHasFixedSize(true);
         mCommentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mCommentRecyclerView.setAdapter(adapter);
 
+        firestore.collection("Posts/"+post_id+"/Comments").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for (DocumentChange documentChange: value.getDocumentChanges()){
+                    if (documentChange.getType()==DocumentChange.Type.ADDED){
+                        Comments comments=documentChange.getDocument().toObject(Comments.class);
+                        mList.add(comments);
+                        adapter.notifyDataSetChanged();
+                    }else{
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+
+            }
+        });
         mAddCommentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/petzzu/Model/Comments.java
+++ app/src/main/java/com/example/petzzu/Model/Comments.java
@@ -1,5 +1,13 @@
 package com.example.petzzu.Model;
 
 public class Comments {
+    private String comment,user;
 
+    public String getComment() {
+        return comment;
+    }
+
+    public String getUser() {
+        return user;
+    }
 }
